b146dc31d38cb236f487d59db8464f3b56fd8a4e
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-04-20 19:39:08 +0200

    /

--- src/main/kotlin/top/abr/demo_spring_kt/DemoSpringKtApplication.kt
+++ src/main/kotlin/top/abr/demo_spring_kt/DemoSpringKtApplication.kt
@@ -1,5 +1,6 @@
 package top.abr.demo_spring_kt
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class DemoSpringKtApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoSpringKtApplication>(*args)
+    runApplication<DemoSpringKtApplication>(*args) {
+        setBannerMode(Banner.Mode.OFF)
+    }
 }
