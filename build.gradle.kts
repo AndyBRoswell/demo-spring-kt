71b5f05901b340229ef8dcfe6976132efcac8af4
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-04-27 12:06:04 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     war
     id("org.springframework.boot") version "3.0.6"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
 }
 
 group = "top.abr"
